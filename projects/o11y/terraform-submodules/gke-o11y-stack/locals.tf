6cd3a297e155975b5b382e0178435f53e1155082
damianagape <damian.agape@gmail.com>
2025-12-04 18:26:42 +0000

    db

--- projects/o11y/terraform-submodules/gke-o11y-stack/locals.tf
+++ projects/o11y/terraform-submodules/gke-o11y-stack/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  clickhouse_endpoint = "tcp://${helm_release.clickhouse.name}.${helm_release.clickhouse.namespace}.svc.cluster.local:9000"
+  clickhouse_endpoint = "tcp://${helm_release.clickhouse.name}-0.${helm_release.clickhouse.name}-headless.${helm_release.clickhouse.namespace}.svc.cluster.local:9000"
 }
 
 data "kubernetes_secret" "grafana_smtp" {
