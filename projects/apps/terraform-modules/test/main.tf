4f0ac9214a97ae188ca56f2709e8afcd82a906f0
damianagape <damian.agape@gmail.com>
2025-12-16 16:55:47 +0000

    ns

--- projects/apps/terraform-modules/test/main.tf
+++ projects/apps/terraform-modules/test/main.tf
@@ -25,3 +25,22 @@ module "test_workspace" {
     "user:dagape.test@gmail.com",
   ]
 }
+
+data "kubernetes_service" "test" {
+  metadata {
+    name      = "goapp-test-7-http-server"
+    namespace = "gomod-test-7"
+  }
+}
+
+module "test_gateway_http_route" {
+  # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-7-private-terraform-modules/gorun/core/k8s-gateway-http-route/0.7.100.zip"
+  source = "../../../core/terraform-submodules/k8s-gateway-http-route"
+
+  kubernetes_service = data.kubernetes_service.test
+
+  domain            = "goapp.gogke-test-7.damianagape.pl"
+  service_port      = 80
+  container_port    = 8080
+  health_check_path = "/healthy"
+}
