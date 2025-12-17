1dfa20737e01cba7944c9e11c8e31b5d886a6f43
damianagape <damian.agape@gmail.com>
2025-12-17 18:28:31 +0000

    mod

--- projects/apps/terraform-modules/test/main.tf
+++ projects/apps/terraform-modules/test/main.tf
@@ -1,18 +1,18 @@
-module "test_vault" {
+module "gomod_test_vault" {
   # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-7-private-terraform-modules/gorun/core/k8s-vault/0.7.100.zip"
   source = "../../../core/terraform-submodules/k8s-vault"
 
   vault_name = "gomod-test-7"
 
   iam_readers = [
-    "serviceAccount:gha-damianagape-gomod@gogcp-main-7.iam.gserviceaccount.com",
   ]
   iam_writers = [
+    "serviceAccount:gha-damianagape-gomod@gogcp-main-7.iam.gserviceaccount.com",
     "user:dagape.test@gmail.com",
   ]
 }
 
-module "test_workspace" {
+module "gomod_test_workspace" {
   # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-7-private-terraform-modules/gorun/core/k8s-workspace/0.7.100.zip"
   source = "../../../core/terraform-submodules/k8s-workspace"
 
@@ -26,18 +26,18 @@ module "test_workspace" {
   ]
 }
 
-data "kubernetes_service" "test" {
+data "kubernetes_service" "goapp_test" {
   metadata {
-    name      = "goapp-test-7-http-server"
+    name      = "goapp-test-7"
     namespace = "gomod-test-7"
   }
 }
 
-module "test_gateway_http_route" {
+module "goapp_test_gateway_http_route" {
   # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-7-private-terraform-modules/gorun/core/k8s-gateway-http-route/0.7.100.zip"
   source = "../../../core/terraform-submodules/k8s-gateway-http-route"
 
-  kubernetes_service = data.kubernetes_service.test
+  kubernetes_service = data.kubernetes_service.goapp_test
 
   domain            = "goapp.gogke-test-7.damianagape.pl"
   service_port      = 80
